f1a8a5f45e5f72a851d5dfb720263756dca02a41
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-12-30 19:44:52 -0300

    Componente de texto.

--- lab-ds/src/components/Text.tsx
+++ lab-ds/src/components/Text.tsx
@@ -1,15 +1,19 @@
+import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx'
 
-interface TextProps {
-  size?: 'sm' | 'md' | 'lg' 
+export interface TextProps {
+  size?: 'sm' | 'md' | 'lg';
+  children: string;
 }
 
-export function Text({ size = 'md' }: TextProps) {
+export function Text({ size = 'md', children }: TextProps) {
   return (
     <span className={clsx('text-gray-100 font-sans', {
       'text-xs': size === 'sm',
       'text-sm': size === 'md',
       'text-md': size === 'lg',
-    })}>Text</span>
+    })}>
+      {children}
+    </span>
   )
 }
