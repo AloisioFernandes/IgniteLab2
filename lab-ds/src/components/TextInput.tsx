b0558c4d3cc4b7bbc6846e91c60a53271fc8d678
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-01-04 21:36:56 -0300

    Estilização padrão input completa.

--- lab-ds/src/components/TextInput.tsx
+++ lab-ds/src/components/TextInput.tsx
@@ -5,11 +5,13 @@ export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 export function TextInput(props: TextInputProps) {
   return (
-    <input 
-      className={clsx(
-        'py-4 px-3 rounded bg-gray-800 w-full text-gray-100 text-xs placeholder:text-gray-400'
-      )}
-      {...props}
-    />
+    <div className='py-4 px-3 rounded bg-gray-800 w-full outline-none text-gray-100 text-xs placeholder:text-gray-400 focus:ring-2 ring-cyan-300'>
+      <input 
+        className={clsx(
+          ''
+        )}
+        {...props}
+      />
+    </div>
   )
 }
