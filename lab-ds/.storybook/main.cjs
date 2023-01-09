ae5c28cb81139ea9a788a2e763f8bf3124cd3058
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-01-09 14:48:13 -0300

    CI deploy.

--- lab-ds/.storybook/main.cjs
+++ lab-ds/.storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignitelab2/lab-ds/'
+    }
+
+    return config
   }
 }
