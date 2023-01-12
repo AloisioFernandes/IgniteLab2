6746f165edfee009d58ed0db607f316fb7e421fe
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-01-12 20:58:43 -0300

    Logo.

--- lab-ds/src/App.tsx
+++ lab-ds/src/App.tsx
@@ -1,13 +1,11 @@
+import { Logo } from './Logo'
 import './styles/global.css'
 
 export function App() {
   return (
-    <>
-      <h1 className="font-bold text-2xl text-violet-500">Hello World</h1>
+    <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100">
+      <Logo />
 
-      <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300">
-        Enviar
-      </button>
-    </>
+    </div>
   )
 }
