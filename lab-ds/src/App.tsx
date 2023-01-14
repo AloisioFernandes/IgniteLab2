30faccb9f93758f824a2a0a3d0acfe7dbb2f5eb8
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-01-14 11:01:52 -0300

    Form.

--- lab-ds/src/App.tsx
+++ lab-ds/src/App.tsx
@@ -1,11 +1,13 @@
+import { Envelope } from 'phosphor-react'
 import { Heading } from './components/Heading'
 import { Text } from './components/Text'
+import { TextInput } from './components/TextInput'
 import { Logo } from './Logo'
 import './styles/global.css'
 
 export function App() {
   return (
-    <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100">
+    <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
       <header className='flex flex-col items-center'>
         <Logo />
 
@@ -17,6 +19,19 @@ export function App() {
           Faça login e comece a usar!
         </Text>
       </header>
+
+      <form className="flex flex-col items-stretch w-full max-w-sm">
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text className="font-semibold">Endereço de e-mail</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+
+            <TextInput.Input placeholder='Digite seu e-mail' />
+          </TextInput.Root>
+        </label>
+      </form>
     </div>
   )
 }
