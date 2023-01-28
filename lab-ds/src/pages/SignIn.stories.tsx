a85e70b126f1677bbdf46200405eeaa77834493e
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-01-28 19:44:47 -0300

    Tratamento de requisições.

--- lab-ds/src/pages/SignIn.stories.tsx
+++ lab-ds/src/pages/SignIn.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from '@storybook/react'
 import { within, userEvent, waitFor } from '@storybook/testing-library'
 import { expect } from '@storybook/jest'
 import { SignIn } from "./SignIn";
+import { rest } from 'msw'
 
 export default {
   title: 'Pages/SignIn',
@@ -10,7 +11,13 @@ export default {
   argTypes: {},
   parameters: {
     msw: {
-      handlers: []
+      handlers: [
+        rest.post('/sessions', (req, res, ctx) => {
+          return res(ctx.json({
+            message: 'Login realizado!'
+          }))
+        })
+      ]
     },
   }
 } as Meta
